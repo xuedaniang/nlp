8fcaaddcc78cc30248ff9c9c706527baab313c25
jingjing328 <41976335+xuedaniang@users.noreply.github.com>
2019-07-31 09:50:37 +0800

    Add files via upload

---
+++ new.py
@@ -0,0 +1,14 @@
+import keras
+from keras.layers.embeddings import Embedding
+
+model = Sequential()
+model.add(Embedding(1000, 64, input_length=10))
+# the model will take as input an integer matrix of size (batch, input_length).
+# the largest integer (i.e. word index) in the input should be no larger than 999 (vocabulary size).
+# now model.output_shape == (None, 10, 64), where None is the batch dimension.
+
+input_array = np.random.randint(1000, size=(32, 10))
+
+model.compile('rmsprop', 'mse')
+output_array = model.predict(input_array)
+assert output_array.shape == (32, 10, 64)
